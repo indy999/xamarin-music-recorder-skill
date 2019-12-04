6728c5e16268bd44d763d764e6cacbe2b654aaaa
Inderpal Ghataore <isghat@hotmail.com>
2019-12-04 09:41:34 +0000

    track mixing working

--- MusicRecorder/MainActivity.cs
+++ MusicRecorder/MainActivity.cs
@@ -8,6 +8,7 @@ using Android.Content.PM;
 using Android.Support.Design.Widget;
 using System.Threading.Tasks;
 using System.IO;
+using System.Linq;
 using Android.Content.Res;
 using System.Threading;
 using Environment = System.Environment;
@@ -75,14 +76,17 @@ namespace MusicRecorder
         async Task MixtrackButton_Click()
         {
             AssetManager assets = this.Assets;
-            long totalBytesTrack1 = 121217;
-            long totalBytesTrack2 = 153271;
-            long totalBytesTrack3 = 117838;
+            long totalBytesTrack1 = 1032000;
+            long totalBytesTrack2 = 1422184;
+            long totalBytesTrack3 = 1016484;
+            short[] track1Short;
+            short[] track2Short;
+            short[] track3Short;
 
             long totalBytes = 37534;
 
-            rate = 11025;
-            audioEncoding = Encoding.Pcm8bit;
+            rate = 48000;
+            audioEncoding = Encoding.Pcm16bit;
 
             BinaryReader binaryReader = new BinaryReader(assets.Open("sample.wav"));
 
@@ -91,25 +95,71 @@ namespace MusicRecorder
             binaryReader.Close();
 
 
-            binaryReader = new BinaryReader(assets.Open("Track 1.m4a"));
+            binaryReader = new BinaryReader(assets.Open("Track 1.wav"));
 
             audioBufferTrack1 = binaryReader.ReadBytes((Int32)totalBytesTrack1);
 
+
+            track1Short = new short[(int)Math.Ceiling((double)audioBufferTrack1.Length / 2)];
+            Buffer.BlockCopy(audioBufferTrack1, 0, track1Short, 0, audioBufferTrack1.Length);
+
+            var track1ShortList = track1Short.ToList();
+
+            var zeroFillArrayLength = totalBytesTrack2 - totalBytesTrack1;
+            short[] zeroFillArray = new short[zeroFillArrayLength];
+
+            track1ShortList.AddRange(zeroFillArray);
+            var reformedTrack1ShortArray = track1ShortList.ToArray();
+            
             binaryReader.Close();
 
-            binaryReader = new BinaryReader(assets.Open("Track 2.m4a"));
+            binaryReader = new BinaryReader(assets.Open("Track 2.wav"));
 
             audioBufferTrack2 = binaryReader.ReadBytes((Int32)totalBytesTrack2);
 
+            track2Short = new short[(int)Math.Ceiling((double)audioBufferTrack2.Length / 2)];
+            Buffer.BlockCopy(audioBufferTrack2, 0, track2Short, 0, audioBufferTrack2.Length);
+
             binaryReader.Close();
 
-            binaryReader = new BinaryReader(assets.Open("Track 3.m4a"));
+            binaryReader = new BinaryReader(assets.Open("Track 3.wav"));
 
             audioBufferTrack3 = binaryReader.ReadBytes((Int32)totalBytesTrack3);
 
+            track3Short = new short[(int)Math.Ceiling((double)audioBufferTrack3.Length / 2)];
+            Buffer.BlockCopy(audioBufferTrack3, 0, track3Short, 0, audioBufferTrack3.Length);
+
+            var track3ShortList = track3Short.ToList();
+
+            zeroFillArrayLength = totalBytesTrack2 - totalBytesTrack3;
+            zeroFillArray = new short[zeroFillArrayLength];
+
+            track3ShortList.AddRange(zeroFillArray);
+            var reformedTrack3ShortArray = track3ShortList.ToArray();
+
             binaryReader.Close();
 
-            await PlayAudioTrack(audioBuffer);
+            var output = new short[track2Short.Length];
+
+            for (int i = 0; i < output.Length; i++)
+            {
+
+                float samplef1 = reformedTrack1ShortArray[i] / 32768.0f;
+                float samplef2 = track2Short[i] / 32768.0f;
+                float samplef3 = reformedTrack3ShortArray[i] / 32768.0f;
+
+                float mixed = samplef1 + samplef2 + samplef3;
+                // reduce the volume a bit:
+                mixed *= (float)0.8;
+                // hard clipping
+                if (mixed > 1.0f) mixed = 1.0f;
+                if (mixed < -1.0f) mixed = -1.0f;
+                short outputSample = (short)(mixed * 32768.0f);
+
+                output[i] = outputSample;
+            }
+
+            await PlayAudioTrack(output);
 
 
         }
@@ -117,19 +167,15 @@ namespace MusicRecorder
         async Task PlaybackButton_Click()
         {
             AssetManager assets = this.Assets;
-            //long totalBytes = 37534;
-
-            //BinaryReader binaryReader = new BinaryReader(assets.Open("sample.wav"));
-
-            //audioBuffer = binaryReader.ReadBytes((Int32)totalBytes);
-
-            //binaryReader.Close();
-            string path = System.Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-
+            
             if (memoryStream.Length > 0)
             {
-                await PlayAudioTrack(memoryStream.ToArray());
-                //File.WriteAllBytes(path+"/test.wav",memoryStream.ToArray());
+                var audioArray = memoryStream.ToArray();
+           
+                var audioArrayShort = new short[(int)Math.Ceiling((double)audioArray.Length / 2)];
+                Buffer.BlockCopy(audioArray, 0, audioArrayShort, 0, audioArray.Length);
+
+                await PlayAudioTrack(audioArrayShort);
             }
         }
 
@@ -168,15 +214,15 @@ namespace MusicRecorder
             await Console.Out.WriteLineAsync("STOP RECORDING EVENT CLICKED");
             endRecording = true;
             //Thread.Sleep(500); // Give it time to drop out.
-            }
+        }
 
-        async Task PlayAudioTrack(byte[] audioBuffer)
+        async Task PlayAudioTrack(short[] audioBuffer)
         {
             var attributes = new AudioAttributes.Builder().SetUsage(AudioUsageKind.Media).SetContentType(AudioContentType.Speech).Build();
-
+            
             audioTrack = new AudioTrack(attributes, new AudioFormat.Builder()
                 .SetEncoding(audioEncoding)
-                .SetChannelMask(ChannelOut.Mono)
+                .SetChannelMask(ChannelOut.Stereo)
                 .SetSampleRate(rate).Build()
                 , audioBuffer.Length,
                 AudioTrackMode.Stream,
@@ -208,7 +254,7 @@ namespace MusicRecorder
                     // Write out the audio file.
                     await memoryStream.WriteAsync(audioBuffer, 0, audioBuffer.Length);
 
-                    await Console.Out.WriteLineAsync("RECORDING SOUND. Memory stream size:"+ memoryStream.Length);   
+                    await Console.Out.WriteLineAsync("RECORDING SOUND. Memory stream size:" + memoryStream.Length);
                 }
                 catch (Exception ex)
                 {
@@ -311,7 +357,7 @@ namespace MusicRecorder
 
         private async Task RequestRecordAudioPermission()
         {
-            
+
             string permission = PermissionsAudio[0];
 
             if (CheckSelfPermission(permission) == Android.Content.PM.Permission.Granted)
