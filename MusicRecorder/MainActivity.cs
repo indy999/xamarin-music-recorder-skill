641de15dc10508fbc9e35a23c134d0af5805b242
Inderpal Ghataore <isghat@hotmail.com>
2019-12-06 15:02:28 +0000

    making mixing dynamic

--- MusicRecorder/MainActivity.cs
+++ MusicRecorder/MainActivity.cs
@@ -3,6 +3,7 @@ using Android.Widget;
 using Android.OS;
 using Android.Media;
 using System;
+using System.Collections.Generic;
 using Android;
 using Android.Content.PM;
 using Android.Support.Design.Widget;
@@ -19,10 +20,7 @@ namespace MusicRecorder
     public class MainActivity : Activity
     {
         public Action<bool> RecordingStateChanged;
-
-        static string filePath = "sample.wav";
-        byte[] buffer = null;
-
+        
         bool endRecording = false;
         bool isRecording = true;
 
@@ -35,12 +33,13 @@ namespace MusicRecorder
 
         const int RequestLocationId = 0;
         Android.Views.View layout;
+        private List<byte[]> audioTracks;
         byte[] audioBuffer;
 
         private byte[] audioBufferTrack1;
         private byte[] audioBufferTrack2;
         private byte[] audioBufferTrack3;
-
+        private TextView mainTextView;
 
         private MemoryStream memoryStream;
 
@@ -59,7 +58,10 @@ namespace MusicRecorder
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
+            audioTracks = new List<byte[]>();
+
             layout = FindViewById<LinearLayout>(Resource.Layout.Main);
+            mainTextView = FindViewById<TextView>(Resource.Id.textView1);
             Button startRecordingButton = FindViewById<Button>(Resource.Id.btnStartRecording);
             Button stopRecordingButton = FindViewById<Button>(Resource.Id.btnStopRecording);
             Button playbackButton = FindViewById<Button>(Resource.Id.btnPlayback);
@@ -75,80 +77,93 @@ namespace MusicRecorder
 
         async Task MixtrackButton_Click()
         {
-            AssetManager assets = this.Assets;
-            long totalBytesTrack1 = 1032000;
-            long totalBytesTrack2 = 1422184;
-            long totalBytesTrack3 = 1016484;
-            short[] track1Short;
-            short[] track2Short;
-            short[] track3Short;
+            //AssetManager assets = this.Assets;
+            //long totalBytesTrack1 = 1032000;
+            //long totalBytesTrack2 = 1422184;
+            //long totalBytesTrack3 = 1016484;
+            //short[] track1Short;
+            //short[] track2Short;
+            //short[] track3Short;
 
-            long totalBytes = 37534;
+            //long totalBytes = 37534;
 
-            rate = 48000;
-            audioEncoding = Encoding.Pcm16bit;
+            //rate = 48000;
+            //audioEncoding = Encoding.Pcm16bit;
 
-            BinaryReader binaryReader = new BinaryReader(assets.Open("sample.wav"));
+            //BinaryReader binaryReader = new BinaryReader(assets.Open("sample.wav"));
 
-            audioBuffer = binaryReader.ReadBytes((Int32)totalBytes);
+            //audioBuffer = binaryReader.ReadBytes((Int32)totalBytes);
 
-            binaryReader.Close();
-
-
-            binaryReader = new BinaryReader(assets.Open("Track 1.wav"));
-
-            audioBufferTrack1 = binaryReader.ReadBytes((Int32)totalBytesTrack1);
+            //binaryReader.Close();
 
 
-            track1Short = new short[(int)Math.Ceiling((double)audioBufferTrack1.Length / 2)];
-            Buffer.BlockCopy(audioBufferTrack1, 0, track1Short, 0, audioBufferTrack1.Length);
+            //binaryReader = new BinaryReader(assets.Open("Track 1.wav"));
 
-            var track1ShortList = track1Short.ToList();
+            //audioBufferTrack1 = binaryReader.ReadBytes((Int32)totalBytesTrack1);
 
-            var zeroFillArrayLength = totalBytesTrack2 - totalBytesTrack1;
-            short[] zeroFillArray = new short[zeroFillArrayLength];
 
-            track1ShortList.AddRange(zeroFillArray);
-            var reformedTrack1ShortArray = track1ShortList.ToArray();
+            //track1Short = new short[(int)Math.Ceiling((double)audioBufferTrack1.Length / 2)];
+            //Buffer.BlockCopy(audioBufferTrack1, 0, track1Short, 0, audioBufferTrack1.Length);
+
+            ////var track1ShortList = track1Short.ToList();
+
+            ////var zeroFillArrayLength = totalBytesTrack2 - totalBytesTrack1;
+            ////short[] zeroFillArray = new short[zeroFillArrayLength];
+
+            ////track1ShortList.AddRange(zeroFillArray);
+            ////var reformedTrack1ShortArray = track1ShortList.ToArray();
             
-            binaryReader.Close();
+            //binaryReader.Close();
 
-            binaryReader = new BinaryReader(assets.Open("Track 2.wav"));
+            //binaryReader = new BinaryReader(assets.Open("Track 2.wav"));
 
-            audioBufferTrack2 = binaryReader.ReadBytes((Int32)totalBytesTrack2);
+            //audioBufferTrack2 = binaryReader.ReadBytes((Int32)totalBytesTrack2);
 
-            track2Short = new short[(int)Math.Ceiling((double)audioBufferTrack2.Length / 2)];
-            Buffer.BlockCopy(audioBufferTrack2, 0, track2Short, 0, audioBufferTrack2.Length);
+            //track2Short = new short[(int)Math.Ceiling((double)audioBufferTrack2.Length / 2)];
+            //Buffer.BlockCopy(audioBufferTrack2, 0, track2Short, 0, audioBufferTrack2.Length);
 
-            binaryReader.Close();
+            //binaryReader.Close();
 
-            binaryReader = new BinaryReader(assets.Open("Track 3.wav"));
+            //binaryReader = new BinaryReader(assets.Open("Track 3.wav"));
 
-            audioBufferTrack3 = binaryReader.ReadBytes((Int32)totalBytesTrack3);
+            //audioBufferTrack3 = binaryReader.ReadBytes((Int32)totalBytesTrack3);
 
-            track3Short = new short[(int)Math.Ceiling((double)audioBufferTrack3.Length / 2)];
-            Buffer.BlockCopy(audioBufferTrack3, 0, track3Short, 0, audioBufferTrack3.Length);
+            //track3Short = new short[(int)Math.Ceiling((double)audioBufferTrack3.Length / 2)];
+            //Buffer.BlockCopy(audioBufferTrack3, 0, track3Short, 0, audioBufferTrack3.Length);
 
-            var track3ShortList = track3Short.ToList();
+            ////var track3ShortList = track3Short.ToList();
 
-            zeroFillArrayLength = totalBytesTrack2 - totalBytesTrack3;
-            zeroFillArray = new short[zeroFillArrayLength];
+            ////zeroFillArrayLength = totalBytesTrack2 - totalBytesTrack3;
+            ////zeroFillArray = new short[zeroFillArrayLength];
 
-            track3ShortList.AddRange(zeroFillArray);
-            var reformedTrack3ShortArray = track3ShortList.ToArray();
+            ////track3ShortList.AddRange(zeroFillArray);
+            ////var reformedTrack3ShortArray = track3ShortList.ToArray();
 
-            binaryReader.Close();
+            //binaryReader.Close();
 
-            var output = new short[track2Short.Length];
+            //iterate through byte array list and convert to short array list
+
+            //set output array length to max length of short array list
+
+            var audioTrackShortList = new List<short[]>();
+
+            foreach (var audioTrack in audioTracks)
+            {
+                var audioTrackShort= new short[(int)Math.Ceiling((double)audioTrack.Length / 2)];
+                Buffer.BlockCopy(audioTrack, 0, audioTrackShort, 0, audioTrack.Length);
+
+                audioTrackShortList.Add(audioTrackShort);
+            }
+
+            var output = new short[audioTrackShortList.Max(x => x.Length)];
+            float samplef1 = 0;
+            float samplef2 = 0;
+            float samplef3 = 0;
 
             for (int i = 0; i < output.Length; i++)
             {
+                float mixed = MixTracks(audioTrackShortList, i);
 
-                float samplef1 = reformedTrack1ShortArray[i] / 32768.0f;
-                float samplef2 = track2Short[i] / 32768.0f;
-                float samplef3 = reformedTrack3ShortArray[i] / 32768.0f;
-
-                float mixed = samplef1 + samplef2 + samplef3;
                 // reduce the volume a bit:
                 mixed *= (float)0.8;
                 // hard clipping
@@ -164,18 +179,35 @@ namespace MusicRecorder
 
         }
 
+        private float MixTracks(List<short[]> audioTrackShortList, int i)
+        {
+            //in mixing loop then create method to iterate through short track array list passig current index
+            // then add array[i] - check for zero and add to mixed float and return
+            float mixed = 0;
+
+            foreach (var audioTrack in audioTrackShortList)
+            {
+                if (i < audioTrack.Length)
+                {
+                    mixed += (audioTrack[i] / 32768.0f); 
+                }
+            }
+
+            return mixed;
+        }
+
         async Task PlaybackButton_Click()
         {
-            AssetManager assets = this.Assets;
-            
             if (memoryStream.Length > 0)
             {
                 var audioArray = memoryStream.ToArray();
            
-                var audioArrayShort = new short[(int)Math.Ceiling((double)audioArray.Length / 2)];
-                Buffer.BlockCopy(audioArray, 0, audioArrayShort, 0, audioArray.Length);
+                //var audioArrayShort = new short[(int)Math.Ceiling((double)audioArray.Length / 2)];
+                //Buffer.BlockCopy(audioArray, 0, audioArrayShort, 0, audioArray.Length);
 
-                await PlayAudioTrack(audioArrayShort);
+                await PlayAudioTrack(audioArray);
+
+                //AudioTrackStop();
             }
         }
 
@@ -213,11 +245,12 @@ namespace MusicRecorder
 
             await Console.Out.WriteLineAsync("STOP RECORDING EVENT CLICKED");
             endRecording = true;
-            //Thread.Sleep(500); // Give it time to drop out.
         }
 
         async Task PlayAudioTrack(short[] audioBuffer)
         {
+            AudioTrackStop();
+
             var attributes = new AudioAttributes.Builder().SetUsage(AudioUsageKind.Media).SetContentType(AudioContentType.Speech).Build();
             
             audioTrack = new AudioTrack(attributes, new AudioFormat.Builder()
@@ -228,9 +261,36 @@ namespace MusicRecorder
                 AudioTrackMode.Stream,
                 1);
 
+            audioTrack.Play();
+            await audioTrack.WriteAsync(audioBuffer, 0, audioBuffer.Length);
+        }
+
+        async Task PlayAudioTrack(byte[] audioBuffer)
+        {
+            AudioTrackStop();
+
+            var attributes = new AudioAttributes.Builder().SetUsage(AudioUsageKind.Media).SetContentType(AudioContentType.Speech).Build();
+
+            audioTrack = new AudioTrack(attributes, new AudioFormat.Builder()
+                    .SetEncoding(audioEncoding)
+                    .SetChannelMask(ChannelOut.Stereo)
+                    .SetSampleRate(rate).Build()
+                , audioBuffer.Length,
+                AudioTrackMode.Stream,
+                1);
+
 
             audioTrack.Play();
             await audioTrack.WriteAsync(audioBuffer, 0, audioBuffer.Length);
+        }
+        public void AudioTrackStop()
+        {
+            if (audioTrack != null)
+            {
+                audioTrack.Stop();
+                audioTrack.Release();
+                audioTrack = null;
+            }
         }
 
         private async Task RecordAudio()
@@ -266,6 +326,12 @@ namespace MusicRecorder
             await Console.Out.WriteLineAsync("We successfully stopped recording.");
             audioRecorder.Stop();
             audioRecorder.Release();
+
+            if (memoryStream.Length > 0)
+            {
+                audioTracks.Add(memoryStream.ToArray());
+                mainTextView.Text = "Numbers of Tracks:" + audioTracks.Count;
+            }
 
             isRecording = false;
 
